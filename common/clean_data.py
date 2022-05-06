29a6e5bb0066b9340b4a749326b223956d713d44
Inkar <willsumerfield@gmail.com>
2022-05-05 20:36:42 -0700

    Add Covid Rates and Visualization

--- common/clean_data.py
+++ common/clean_data.py
@@ -16,17 +16,17 @@ if __name__ == '__main__':
 
     # region Open the files of each dataset
 
-    case_df = pd.read_csv('data/raw_csvs/Case.csv')
-    patient_df = pd.read_csv('data/raw_csvs/PatientInfo.csv')
-    policy_df = pd.read_csv('data/raw_csvs/Policy.csv')
-    region_df = pd.read_csv('data/raw_csvs/Region.csv')
-    search_trend_df = pd.read_csv('data/raw_csvs/SearchTrend.csv')
-    #seoul_df = pd.read_csv('data/raw_csvs/SeoulFloating.csv')
-    test_df = pd.read_csv('data/raw_csvs/Time.csv')
-    age_df = pd.read_csv('data/raw_csvs/TimeAge.csv')
-    gender_df = pd.read_csv('data/raw_csvs/TimeGender.csv')
-    province_df = pd.read_csv('data/raw_csvs/TimeProvince.csv')
-    weather_df = pd.read_csv('data/raw_csvs/Weather.csv')
+    case_df = pd.read_csv('../data/raw_csvs/Case.csv')
+    patient_df = pd.read_csv('../data/raw_csvs/PatientInfo.csv')
+    policy_df = pd.read_csv('../data/raw_csvs/Policy.csv')
+    region_df = pd.read_csv('../data/raw_csvs/Region.csv')
+    search_trend_df = pd.read_csv('../data/raw_csvs/SearchTrend.csv')
+    #seoul_df = pd.read_csv('../data/raw_csvs/SeoulFloating.csv')
+    test_df = pd.read_csv('../data/raw_csvs/Time.csv')
+    age_df = pd.read_csv('../data/raw_csvs/TimeAge.csv')
+    gender_df = pd.read_csv('../data/raw_csvs/TimeGender.csv')
+    province_df = pd.read_csv('../data/raw_csvs/TimeProvince.csv')
+    weather_df = pd.read_csv('../data/raw_csvs/Weather.csv')
 
     # endregion Open the files of each dataset
 
@@ -37,7 +37,7 @@ if __name__ == '__main__':
     regions = province_df['province'].unique()
 
     # Store the list of regions as a pickled list
-    with open('data/region_list.pkl', 'wb') as file: pickle.dump(regions, file)
+    with open('../data/region_list.pkl', 'wb') as file: pickle.dump(regions, file)
 
     # For each region, make a dataframe, a file, and save each
     for region in regions:
@@ -48,25 +48,31 @@ if __name__ == '__main__':
         df.index.name = 'index'
         
         # Store the case counts over time as a csv
-        df.to_csv('data/clean_csvs/absolute/' + region + '.csv')
+        df.to_csv('../data/clean_csvs/absolute/' + region + '.csv')
 
         # Pickle the case counts over time
-        df.to_pickle('data/dataframes/absolute/' + region + '.pkl')
+        df.to_pickle('../data/dataframes/absolute/' + region + '.pkl')
 
         # Change the dataframe into changes in cases over time
-        for i in range(1, df.shape[0]):
-            
-            # Set the columns
+        df_rates = df.copy()
+        for i in range(1, df.shape[0]): df_rates.iloc[i, :] = df.iloc[i, :] - df.iloc[i-1, :]
+        df_rates.iloc[0, :] = df_rates.iloc[1, :]
+
+        # Store the case counts over time as a csv
+        df_rates.to_csv('../data/clean_csvs/relative/' + region + '.csv')
+
+        # Pickle the case counts over time
+        df_rates.to_pickle('../data/dataframes/relative/' + region + '.pkl')
 
     # Create a dataframe of all summed cases
-    total_df = test_df.loc[:,'confirmed':'deceased']
+    total_df = test_df.loc[:, 'confirmed':'deceased']
     total_df.reset_index(drop=True, inplace=True)
     total_df.index.name = 'index'
 
     # Store the dataframe as a csv
-    total_df.to_csv('data/clean_csvs/absolute/Total.csv')
+    total_df.to_csv('../data/clean_csvs/absolute/Total.csv')
 
     # Pickle the dataframe
-    total_df.to_pickle('data/dataframes/absolute/Total.pkl')
+    total_df.to_pickle('../data/dataframes/absolute/Total.pkl')
 
     # region Create Regional Datasets
